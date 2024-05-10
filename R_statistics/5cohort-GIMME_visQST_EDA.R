9b330f255c2434772b5d10e9c968f641d4b4126e
ncwaller <123313032+ncwaller@users.noreply.github.com>
2024-05-10 16:42:14 -0400

    Update 5cohort-GIMME_visQST_EDA.R

--- R_statistics/5cohort-GIMME_visQST_EDA.R
+++ R_statistics/5cohort-GIMME_visQST_EDA.R
@@ -178,9 +178,12 @@ boxplot(data_full$fm_score_bsl~data_full$responder_f,
 boxplot(data_full$vis_bright_avg~data_full$responder_f,
         col = c("red", "darkgreen"), 
         boxwex = .5,                              
-        ylab = "FM Score",     
+        ylab = "Vis Metric",     
         xlab = "Responder",    
         ylim = c(0,100)) 
+
+# T Test
+t.test(vis06_unpl_avg ~ responder_f, data = data_full)
 
 # CORRELATIONS
 ## Pearson: Multiple Functions 
@@ -206,3 +209,67 @@ data_rcortable$P
 View(data_rcortable$P)
 
 
+
+# Linear Mixed Model template
+## Designed for any .csv file, organized by groups of interest in labelled columns
+
+# ============================================================================== 
+
+install.packages("lme4")
+install.packages("merDerive")
+install.packages("ggeffects")
+
+library(lme4) 
+library(merDeriv) 
+library(ggeffects) 
+
+cohortf <- factor(data_full$cohort, levels = c(1:5), labels = c("HC", "CTS", "RA", "OA", "FM"))
+illuminance_levelf <- factor(data_full$illuminance_level, levels = c(1:6), labels = c("1", "2", "3", "4", "5", "6"))
+
+#cohort effect
+data_full.null = lmer(bright_rating ~ sex + age + illuminance_level +
+                         (1|subid), data=data_full, REML=FALSE)
+
+data_full.model = lmer(bright_rating ~ sex + age + cohortf + illuminance_level +
+                         (1|subid), data=data_full, REML=FALSE)
+data_full.model
+
+anova(data_full.null,data_full.model) #compare the two models (effect of cohort)
+
+coef(data_full.model)
+
+#illuminance effect
+data_full.null = lmer(bright_rating ~ sex + age + cohortf +
+                        (1|subid), data=data_full, REML=FALSE)
+
+data_full.model = lmer(bright_rating ~ sex + age + cohortf + illuminance_level +
+                         (1|subid), data=data_full, REML=FALSE)
+data_full.model
+
+anova(data_full.null,data_full.model) #compare the two models (effect of illuminance)
+
+
+
+#interaction effect of illuminance x group
+data_full.null = lmer(bright_rating ~ sex + age + cohortf + illuminance_level +
+                        (1|subid), data=data_full, REML=FALSE)
+
+data_full.model = lmer(bright_rating ~ sex + age + cohortf * illuminance_level +
+                         (1|subid), data=data_full, REML=FALSE)
+data_full.model
+
+anova(data_full.null,data_full.model) #compare the two models (effect of illuminance)
+
+
+#full, all-in-one model
+data_full.model = lmer(bright_rating ~ sex + age + cohortf + cohortf * illuminance_levelf +
+                         (1|subid), data=data_full, REML=FALSE)
+
+data_full.model
+coef(data_full.model)
+
+library(emmeans)
+options(max.print = 999999999)
+emmeans(data_full.model, list(pairwise ~ cohortf), adjust = "tukey")
+emmeans(data_full.model, list(pairwise ~ illuminance_levelf), adjust = "tukey")
+emmeans(data_full.model, list(pairwise ~ cohortf * illuminance_levelf), adjust = "tukey")
