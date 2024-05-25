27ef1af9fe0a0c68b4b0c7c1db382fc72695593b
ncwaller <123313032+ncwaller@users.noreply.github.com>
2024-05-25 10:17:25 -0400

    Update 7cohort-GIMME_visQST_LMM.R

--- R_statistics/7cohort-GIMME_visQST_LMM.R
+++ R_statistics/7cohort-GIMME_visQST_LMM.R
@@ -13,13 +13,14 @@
 ### Seperate by rating modality, depending on research question
 setwd("/Users/noahwaller/Documents/3cohort-GIMME PAPER/csv_for-code")
 
-data_lmm <- data.frame(read.csv("7cohort_visQST_bright_forLMM.csv", 
+data_lmm <- data.frame(read.csv("7cohort_visQST_bright_respvnon_forLMM.csv", 
                                  header = T, sep = ","))
 View(data_lmm)
 
 ## Format sex and cohort as a factor
 data_lmm$sex_f <- factor(data_lmm$sex, levels=c(0:1), labels=c("Male", "Female"))
 data_lmm$cohort_f <- factor(data_lmm$cohort, levels=c(0:6), labels=c("HC", "RA", "CTS", "OA", "FM", "PSA", "CPP"))
+data_lmm$responder_f <- factor(data_lmm$responder_bin, levels=c(0:3), labels=c("Non-responder", "Responder", "HC", "FM"))
 
 ## Convert to Long Format
 #install.packages("tidyr", repos='http://cran.us.r-project.org')
@@ -37,7 +38,7 @@ data_lmm_long = data_lmm_long[order(data_lmm_long$subid),] # sort by subid
 View(data_lmm_long)
 
 # Remove CTS (causing problems with such low N)
-# data_lmm_long = data_lmm_long[!data_lmm_long$cohort_f=="CTS",]
+data_lmm_long = data_lmm_long[!data_lmm_long$cohort_f=="CTS",]
 
 # INSTALL
 #install.packages("lme4", repos='http://cran.us.r-project.org')
@@ -76,6 +77,20 @@ data_lmm_long_2group = data_lmm_long[data_lmm_long$cohort_f=="FM" | data_lmm_lon
 data_lmm_long_2group = data_lmm_long[data_lmm_long$cohort_f=="FM" | data_lmm_long$cohort_f=="CPP",] #
 
 data_lmm_long_2group = data_lmm_long[data_lmm_long$cohort_f=="PSA" | data_lmm_long$cohort_f=="CPP",] #
+
+
+View(data_lmm_long_2group)
+
+
+# Subframe into 2 Cohorts (Responder Splits)
+data_lmm_long_2group = data_lmm_long[data_lmm_long$responder_f=="HC" | data_lmm_long$responder_f=="FM",] # Sig. both
+data_lmm_long_2group = data_lmm_long[data_lmm_long$responder_f=="HC" | data_lmm_long$responder_f=="Non-responder",] # Sig. group
+data_lmm_long_2group = data_lmm_long[data_lmm_long$responder_f=="HC" | data_lmm_long$responder_f=="Responder",] # NS
+
+data_lmm_long_2group = data_lmm_long[data_lmm_long$responder_f=="FM" | data_lmm_long$responder_f=="Non-responder",] # Close group, sig. interaction
+data_lmm_long_2group = data_lmm_long[data_lmm_long$responder_f=="FM" | data_lmm_long$responder_f=="Responder",] # Sig. both
+
+data_lmm_long_2group = data_lmm_long[data_lmm_long$responder_f=="Non-responder" | data_lmm_long$responder_f=="Responder",] # p = 0.061 for group
 
 
 View(data_lmm_long_2group)
@@ -134,6 +149,42 @@ data_lmm_long_2group.model # check model
 anova(data_lmm_long_2group.null,data_lmm_long_2group.model) #compare the two models (effect of illuminance between groups)
 
 
+
+########################################################################################################
+
+
+# Effect of Responder Status
+
+data_lmm_long_2group.null = lmer(rating ~ sex + age + illuminance_level +
+                        (1|subid), data=data_lmm_long_2group, REML=FALSE)
+                        # add age and sex as fixed effects, and establish null model
+                
+data_lmm_long_2group.model = lmer(rating ~ sex + age + illuminance_level + responder_f +
+                        (1|subid), data=data_lmm_long_2group, REML=FALSE)
+                        # add cohort fixed effect
+                        
+data_lmm_long_2group.model # check model
+anova(data_lmm_long_2group.null,data_lmm_long_2group.model) #compare the two models (effect of cohort between groups)
+
+
+# Effect of Interaction Between Responder Status and Illuminance Level
+
+data_lmm_long_2group.null = lmer(rating ~ sex + age + illuminance_level + responder_f +
+                        (1|subid), data=data_lmm_long_2group, REML=FALSE) 
+
+data_lmm_long_2group.model = lmer(rating ~ sex + age + illuminance_level * responder_f +
+                        (1|subid), data=data_lmm_long_2group, REML=FALSE) 
+                        # add interaction term between illuminance level and cohort
+                        # includes a random effect for subjects and fixed effect for illuminance level
+
+data_lmm_long_2group.model # check model
+anova(data_lmm_long_2group.null,data_lmm_long_2group.model) #compare the two models (effect of interaction between groups)
+
+coef(data_lmm_long_2group.model) # check coefficients
+
+
+
+########################################################################################################
 
 
 #full, all-in-one model
