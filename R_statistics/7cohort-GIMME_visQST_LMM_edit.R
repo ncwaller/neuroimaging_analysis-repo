58542d78620facba0eeb1f8dfe257d79190a4ead
ncwaller <123313032+ncwaller@users.noreply.github.com>
2024-09-19 15:02:47 -0400

    Update 7cohort-GIMME_visQST_LMM_edit.R

--- R_statistics/7cohort-GIMME_visQST_LMM_edit.R
+++ R_statistics/7cohort-GIMME_visQST_LMM_edit.R
@@ -13,14 +13,17 @@
 ### Seperate by rating modality, depending on research question
 setwd("/Users/noahwaller/Documents/VISUAL-QST-7cohort PAPER/csv_for-code")
 
-data_lmm <- data.frame(read.csv("visqst_unpl-only_outrem_forLMM.csv", 
+data_lmm <- data.frame(read.csv("visqst_unpl-only_outrem_forLMM.csv", # unpleasantness
+                                 header = T, sep = ","))
+
+data_lmm <- data.frame(read.csv("visqst_bright-only_outrem_forLMM.csv", # brightness
                                  header = T, sep = ","))
 View(data_lmm)
 
 ## Format sex and cohort as a factor
 data_lmm$sex_f <- factor(data_lmm$sex, levels=c(1:2), labels=c("Male", "Female"))
 data_lmm$cohort_f <- factor(data_lmm$cohort, levels=c(0:6), labels=c("HC", "RA", "CTS", "OA", "CPP", "PSA", "FM"))
-#data_lmm$responder_f <- factor(data_lmm$responder_bin, levels=c(0:3), labels=c("Non-responder", "Responder", "HC", "FM"))
+data_lmm$responder_f <- factor(data_lmm$responder_bin, levels=c(0:3), labels=c("Non-responder", "Responder", "HC", "FM"))
 
 ## Convert to Long Format
 #install.packages("tidyr", repos='http://cran.us.r-project.org')
@@ -32,7 +35,11 @@ library(tidyr)
 # - value: Name of new value column
 # - ...: Names of source columns that contain values
 # - factor_key: Treat the new key column as a factor (instead of character vector)
-data_lmm_long <- gather(data_lmm, illuminance_level, rating, vis01_unpl_avg:vis06_unpl_avg, factor_key=TRUE)
+
+data_lmm_long <- gather(data_lmm, illuminance_level, rating, vis01_unpl_avg:vis06_unpl_avg, factor_key=TRUE) # unpleasantness
+
+data_lmm_long <- gather(data_lmm, illuminance_level, rating, vis01_bright_avg:vis06_bright_avg, factor_key=TRUE) # brightness
+
 data_lmm_long = data_lmm_long[order(data_lmm_long$subid),] # sort by subid
 
 View(data_lmm_long)
@@ -60,7 +67,11 @@ library(ggeffects)
                         # won't work given number of observations (it's too complicated for the dataset)
 
 
-# Full Model
+#########################################################################################################
+# COHORT #
+#########################################################################################################
+
+# Full Model (Cohort)
 full_model = lmer(rating ~ sex + age + cohort_f + illuminance_level +
                          (1|subid), data=data_lmm_long, REML=FALSE)
 anova(full_model)
@@ -114,3 +125,63 @@ summary(post.hoc)
 emmeans(full_model, list(pairwise ~ cohort_f), adjust = "tukey")
 emmeans(full_model, list(pairwise ~ illuminance_level), adjust = "tukey")
 emmeans(full_model, list(pairwise ~ cohort_f * illuminance_level), adjust = "tukey")
+
+
+#########################################################################################################
+# TX RESPONSE #
+#########################################################################################################
+
+# Full Model (Tx Response)
+full_model = lmer(rating ~ sex + age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+anova(full_model)
+coef(full_model)
+confint(full_model, level=0.95)
+
+
+# Test for Significance of Main Effects/Interaction Effects (NOT Multi Comp)
+# Cohort (Main Effect)
+data_lmm_long.null = lmer(rating ~ sex + age + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+# Illuminance (Main Effect)
+data_lmm_long.null = lmer(rating ~ sex + age + responder_f +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+# Interaction (Cohort X Illuminance)
+data_lmm_long.null = lmer(rating ~ sex+  age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + responder_f * illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+
+# Multiple Pairwise Comparisons
+#install.packages("multcomp", repos='http://cran.us.r-project.org')
+#install.packages("emmeans", repos='http://cran.us.r-project.org')
+library(multcomp)
+library(emmeans)
+
+# running glht()
+post.hoc <- glht(full_model, linfct = mcp(responder_f = 'Tukey'))
+summary(post.hoc)
+
+# running emmeans
+emmeans(full_model, list(pairwise ~ responder_f), adjust = "tukey")
+emmeans(full_model, list(pairwise ~ illuminance_level), adjust = "tukey")
+emmeans(full_model, list(pairwise ~ responder_f * illuminance_level), adjust = "tukey")
