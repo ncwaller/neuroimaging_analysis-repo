c6fcdfb6ebf1a233583f875c210393289b94a06d
ncwaller <123313032+ncwaller@users.noreply.github.com>
2024-09-27 12:38:40 -0400

    Update 7cohort-GIMME_visQST_LMM_edit.R

--- R_statistics/7cohort-GIMME_visQST_LMM_edit.R
+++ R_statistics/7cohort-GIMME_visQST_LMM_edit.R
@@ -13,10 +13,10 @@
 ### Seperate by rating modality, depending on research question
 setwd("/Users/noahwaller/Documents/VISUAL-QST-7cohort PAPER/csv_for-code")
 
-data_lmm <- data.frame(read.csv("visqst_unpl-only_tx-resp_outrem_forLMM.csv", # unpleasantness
+data_lmm <- data.frame(read.csv("visqst_unpl-only_tx-resp_cov-fmscore_outrem_forLMM.csv", # unpleasantness
                                  header = T, sep = ","))
 
-data_lmm <- data.frame(read.csv("visqst_bright-only_tx-resp_outrem_forLMM.csv", # brightness
+data_lmm <- data.frame(read.csv("visqst_bright-only_tx-resp_cov-fmscore_outrem_forLMM.csv", # brightness
                                  header = T, sep = ","))
 View(data_lmm)
 
@@ -145,6 +145,79 @@ data_lmm_long.null = lmer(rating ~ sex + age + illuminance_level +
                          (1|subid), data=data_lmm_long, REML=FALSE)
 
 data_lmm_long.model = lmer(rating ~ sex + age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+# Illuminance (Main Effect)
+data_lmm_long.null = lmer(rating ~ sex + age + responder_f +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+# Interaction (Cohort X Illuminance)
+data_lmm_long.null = lmer(rating ~ sex+  age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + responder_f * illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+
+# Multiple Pairwise Comparisons
+#install.packages("multcomp", repos='http://cran.us.r-project.org')
+#install.packages("emmeans", repos='http://cran.us.r-project.org')
+library(multcomp)
+library(emmeans)
+
+# running glht()
+post.hoc <- glht(full_model, linfct = mcp(responder_f = 'Tukey'))
+summary(post.hoc)
+
+# running emmeans
+emmeans(full_model, list(pairwise ~ responder_f), adjust = "tukey")
+emmeans(full_model, list(pairwise ~ illuminance_level), adjust = "tukey")
+emmeans(full_model, list(pairwise ~ responder_f * illuminance_level), adjust = "tukey")
+
+
+
+
+
+#########################################################################################################
+# TX RESPONSE - INCLUDING FM SCORE #
+#########################################################################################################
+
+# Full Model (Tx Response)
+full_model = lmer(rating ~ sex + age + fm_score_bsl + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+anova(full_model)
+coef(full_model)
+confint(full_model, level=0.95)
+
+
+# Test for Significance of Main Effects/Interaction Effects (NOT Multi Comp)
+# Cohort (Main Effect)
+data_lmm_long.null = lmer(rating ~ sex + age + fm_score_bsl + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + fm_score_bsl + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model
+anova(data_lmm_long.null,data_lmm_long.model)
+
+# FM Score (Main Effect)
+data_lmm_long.null = lmer(rating ~ sex + age + responder_f + illuminance_level +
+                         (1|subid), data=data_lmm_long, REML=FALSE)
+
+data_lmm_long.model = lmer(rating ~ sex + age + fm_score_bsl + responder_f + illuminance_level +
                          (1|subid), data=data_lmm_long, REML=FALSE)
 
 data_lmm_long.model
